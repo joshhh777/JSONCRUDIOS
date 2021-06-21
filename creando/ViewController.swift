03dc97dfe163bfa5e214f9c9ea37e1e28b1f7338
Jose Cancino <jose.cancino@tecsup.edu.pe>
2021-06-20 20:14:18 -0500

    Initial Commit

---
+++ creando/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  creando
+//
+//  Created by Mac 09 on 6/20/21.
+//  Copyright © 2021 Mac 09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
